6705022607e27a2bc9ce96e2e4fd10ee82104e7e
Lauren <lat.mail.lat@gmail.com>
2024-03-12 12:12:54 +0000

    created a very basic very ugly hard coded weather card component

---
+++ src/WeatherCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const WeatherCard = () => {
+
+    return(
+        <div>
+            <h1>Weather for today is</h1>
+            <h2>9 degrees</h2>
+            <h2>which is colder than yesterday</h2>
+        </div>
+    )
+
+}
+
+export default WeatherCard;
